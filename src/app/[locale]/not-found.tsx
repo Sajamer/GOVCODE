adf6455184647a259a9b360b992354f2cc13200e
moustafa tlais <moustafa.a.tlais@gmail.com>
2024-12-16 11:58:01 +0200

    added not found page

---
+++ src/app/[locale]/not-found.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
+export default function NotFound(): JSX.Element {
+  const router = useRouter()
+  return (
+    <section className="flex h-screen w-full flex-col items-center justify-center">
+      <Image
+        src={'/assets/icons/404.svg'}
+        width={533}
+        height={564}
+        alt="not-found"
+        priority
+      />
+      <h1 className="text-4xl font-semibold text-gray-600">Oops!!</h1>
+      <p className="mt-3 text-gray-500">
+        This page you are looking for could not be found.
+      </p>
+      <Button
+        variant={'outline'}
+        className="mt-6 h-14 w-fit px-6"
+        onClick={() => router.push('/')}
+      >
+        Go Back to Home
+      </Button>
+    </section>
+  )
+}
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,34 @@
 'use client'
 
-import Error from 'next/error'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
-// Render the default Next.js 404 page when a route
-// is requested that doesn't match the middleware and
-// therefore doesn't have a locale associated with it.
+export default function NotFound(): JSX.Element {
+  const router = useRouter()
 
-export default function NotFound() {
   return (
     <html lang="en">
       <body>
-        <Error statusCode={404} />
+        <section className="flex h-screen w-full flex-col items-center justify-center">
+          <Image
+            src={'/assets/icons/404.svg'}
+            width={533}
+            height={564}
+            alt="not-found"
+            priority
+          />
+          <h1 className="text-4xl font-semibold text-gray-600">Oops!!</h1>
+          <p className="mt-3 text-gray-500">
+            This page you are looking for could not be found.
+          </p>
+          <Button
+            className="mt-6 h-14 w-fit px-6"
+            onClick={() => router.push('/')}
+          >
+            Go Back to Home
+          </Button>
+        </section>
       </body>
     </html>
   )
