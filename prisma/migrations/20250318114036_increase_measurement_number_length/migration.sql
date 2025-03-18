b026e2ec87d76acc500d4bcf9b56ed552460e7a9
Moustafa Tlais <moustafa@techquarters.com.au>
2025-03-18 13:41:48 +0200

    increase the characters of attributes

---
+++ prisma/migrations/20250318114036_increase_measurement_number_length/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE `kpi` MODIFY `measurementNumerator` VARCHAR(512) NULL,
+    MODIFY `measurementDenominator` VARCHAR(512) NULL,
+    MODIFY `measurementNumber` VARCHAR(512) NULL;
