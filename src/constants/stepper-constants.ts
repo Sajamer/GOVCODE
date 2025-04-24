5bdb1dd563bd3d91efa31ce10ec862c25e3a70ea
Moustafa Tlais <moustafa.a.tlais@gmail.com>
2025-04-24 13:30:10 +0300

    change description

--- src/constants/stepper-constants.ts
+++ src/constants/stepper-constants.ts
@@ -2,17 +2,16 @@ export const INDICATOR_FORM_STEPS = [
   {
     id: 'basic-info',
     title: 'Basic Information',
-    description: 'Enter indicator basic information'
+    description: 'Enter indicator basic information',
   },
   {
     id: 'level-config',
     title: 'Level Configuration',
-    description: 'Configure indicator levels'
+    description: 'Configure indicator levels',
   },
   {
     id: 'fields-config',
     title: 'Fields Configuration',
-    description: 'Configure indicator fields'
-  }
-
+    description: 'Configure fields for each level',
+  },
 ]
